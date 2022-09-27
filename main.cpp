2f60ba789530ac6274cce85e8f5c5a1d8176f929
Kyuwoong Lee <kwooler@gmail.com>
2022-09-26 20:37:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
-// Rewrite the following program. Use a switch statement instead of the if/else if statement.
+// Complete this program
 
 #include <iostream>
 using namespace std;
-int main() 
-{
-        int selection;
-        cout << "Which formula do you want to see?\n\n";
-        cout << "1. Area of a circle\n";
-        cout << "2. Area of a rectangle\n";
-        cout << "3. Area of a cylinder\n" ;
-        cout << "4. None of them!\n";
-        cin >> selection;
-//*        if (selection == 1)
-//*                cout << "Pi times radius squared\n";
-//*        else if (selection == 2)
-//*                cout << "Length times width\n";
-//*        else if (selection == 3)
-//*                cout << "Pi times radius squared times height\n";
-//*        else if (selection == 4)
-//*                cout << "Well okay then, good bye!\n";
-//*        else
-//*                cout < < < < "Not good with numbers, eh?\n";
 
+int main()
+{
+	int choice;
+	double width, height, radius;
+	double area;
+
+	cout << "Geometry Calculator";
+	cout << "\t1. Calculate the Area of Circle\n";
+	cout << "\t2. Calculate the Area of Rectangle\n";
+	cout << "\t3. Calculate the Area of Triangle\n";
+	cout << "\t4. Quit\n\n";
+	cout << "Enter your choice (1-4): ";
+	cin >> choice;
+
+        //************************
+        // Your code 
+        //************************
+        
+        cout << area;
 }
